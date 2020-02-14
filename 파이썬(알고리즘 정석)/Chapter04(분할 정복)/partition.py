f08bcabc34000666d3466ea8d3869ed0b883f7ad
pythonjava0218 <54443194+pythonjava0218@users.noreply.github.com>
2020-02-14 22:40:08 +0900

    Add files via upload

---
+++ 파이썬(알고리즘 정석)/Chapter04(분할 정복)/partition.py
@@ -0,0 +1,39 @@
+# 두 요소의 위치를 바꿔주는 helper function
+# 두 요소의 위치를 바꿔주는 helper function
+def swap_elements(my_list, index1, index2):
+    # 코드를 작성하세요.
+    my_list[index1], my_list[index2] = my_list[index2], my_list[index1]
+
+
+# 퀵 정렬에서 사용되는 partition 함수
+def partition(my_list, start, end):
+    # 코드를 작성하세요.
+    pivot = end
+
+    '''
+    기준점 보다 큰 값 = 오른쪽
+    기준점 보다 작은 값 = 왼쪽
+    '''
+    b = 0
+
+    for i in range(start, end + 1):
+
+        if my_list[i] < my_list[pivot]:
+            swap_elements(my_list, i, b)
+            b += 1
+
+    swap_elements(my_list, b, pivot)
+    pivot = b
+
+    return pivot
+# 테스트 1
+list1 = [4, 3, 6, 2, 7, 1, 5]
+pivot_index1 = partition(list1, 0, len(list1) - 1)
+print(list1)
+print(pivot_index1)
+
+# 테스트 2
+list2 = [6, 1, 2, 6, 3, 5, 4]
+pivot_index2 = partition(list2, 0, len(list2) - 1)
+print(list2)
+print(pivot_index2)
