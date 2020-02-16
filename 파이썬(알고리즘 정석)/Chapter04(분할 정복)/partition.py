92b8c7dee4853d4e3e0e8474fcdede032eeee91b
pythonjava0218 <54443194+pythonjava0218@users.noreply.github.com>
2020-02-16 23:33:56 +0900

    Add files via upload

--- 파이썬(알고리즘 정석)/Chapter04(분할 정복)/partition.py
+++ 파이썬(알고리즘 정석)/Chapter04(분할 정복)/partition.py
@@ -1,4 +1,5 @@
 # 두 요소의 위치를 바꿔주는 helper function
+# 두 요소의 위치를 바꿔주는 helper function
 def swap_elements(my_list, index1, index2):
     # 코드를 작성하세요.
     my_list[index1], my_list[index2] = my_list[index2], my_list[index1]
@@ -13,7 +14,8 @@ def partition(my_list, start, end):
     기준점 보다 큰 값 = 오른쪽
     기준점 보다 작은 값 = 왼쪽
     '''
-    b = start
+    b = 0
+
     for i in range(start, end):
 
         if my_list[i] <= my_list[pivot]:
@@ -21,17 +23,18 @@ def partition(my_list, start, end):
             b += 1
 
     swap_elements(my_list, b, pivot)
-    pivot = b
-
-    return pivot
+    return b
 # 테스트 1
-list1 = [4, 3, 6, 2, 7, 1, 5]
-pivot_index1 = partition(list1, 0, len(list1) - 1)
-print(list1)
-print(pivot_index1)
+# list1 = [4, 3, 6, 2, 7, 1, 5]
+
+# list1 = [1, 3, 5, 7, 9, 11, 13, 11]
+# pivot_index1 = partition(list1, 0, len(list1) - 1)
+# print(list1)
+# print(pivot_index1)
 
 # 테스트 2
-list2 = [6, 1, 2, 6, 3, 5, 4]
+# list2 = [6, 1, 2, 6, 3, 5, 4]
+list2 = [1, 3, 2, 4, 6, 5, 6]
 pivot_index2 = partition(list2, 0, len(list2) - 1)
 print(list2)
 print(pivot_index2)
